c57e539eb9d66eb16524c0ccd515ba4e005311d4
Cronin-Golomb <mcroning@pax052.pax.tufts.edu>
2026-05-22 10:33:33 -0400

    Add public solver module

--- src/lc_soliton/api.py
+++ src/lc_soliton/api.py
@@ -7,7 +7,5 @@ This module initially wraps the validated legacy engine with minimal refactoring
 from .config import *
 from .legacy_validated.lc_core.gpu_context import *
 from .io import *
-from .legacy_validated.lc_core.static_solver import *
-from .legacy_validated.lc_core.static_z_march import *
-from .legacy_validated.lc_core.td_runner import *
+from .solvers import *
 from .legacy_validated.lc_core.stability_td import *
---
+++ src/lc_soliton/solvers.py
@@ -0,0 +1,10 @@
+"""
+Static and time-dependent LC solver entry points.
+"""
+
+from .legacy_validated.lc_core.static_solver import *
+from .legacy_validated.lc_core.static_z_march import *
+from .legacy_validated.lc_core.td_runner import *
+from .legacy_validated.lc_core.td_split import *
+from .legacy_validated.lc_engine_validated import *
+
