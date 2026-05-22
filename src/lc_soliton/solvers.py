63e4a8fadd55caeb0c80e03bf4603c3d7262f1ed
Cronin-Golomb <mcroning@pax052.pax.tufts.edu>
2026-05-22 10:35:24 -0400

    Avoid exposing td_split legacy dependency

--- src/lc_soliton/solvers.py
+++ src/lc_soliton/solvers.py
@@ -5,6 +5,5 @@ Static and time-dependent LC solver entry points.
 from .legacy_validated.lc_core.static_solver import *
 from .legacy_validated.lc_core.static_z_march import *
 from .legacy_validated.lc_core.td_runner import *
-from .legacy_validated.lc_core.td_split import *
 from .legacy_validated.lc_engine_validated import *
 
