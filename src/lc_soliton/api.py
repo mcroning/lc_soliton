5d1cbdcd33fef08492a5a62dd05aa4125a2075bf
Cronin-Golomb <mcroning@pax052.pax.tufts.edu>
2026-05-22 10:37:29 -0400

    Add public diagnostics and plotting modules

--- src/lc_soliton/api.py
+++ src/lc_soliton/api.py
@@ -5,7 +5,7 @@ This module initially wraps the validated legacy engine with minimal refactoring
 """
 
 from .config import *
-from .legacy_validated.lc_core.gpu_context import *
 from .io import *
 from .solvers import *
-from .legacy_validated.lc_core.stability_td import *
+from .diagnostics import *
+from .plotting import *
---
+++ src/lc_soliton/plotting.py
@@ -0,0 +1,8 @@
+"""
+Plotting and visualization helpers.
+"""
+
+from .legacy_validated.lc_core.plotting import *
+from .legacy_validated.lc_core.preview import *
+from .legacy_validated.lc_core.notebook_viz import *
+
