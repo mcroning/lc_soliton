549df65dda0c1d81b4fba859bc0d324cddf26e4d
Mark Cronin-Golomb <mark.cronin-golomb@tufts.edu>
2026-05-24 12:25:15 -0400

    Show available engine modes in GUI

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 import streamlit as st
 
-from lc_soliton import LCParams, run_static, load_reference_case
+from lc_soliton import LCParams, run_static, load_reference_case, available_engine_modes
 
 
 
@@ -19,6 +19,11 @@ st.set_page_config(
 )
 
 st.title("LC Soliton Simulator")
+
+st.caption(
+    "Available engine modes: "
+    + ", ".join(available_engine_modes())
+)
 
 st.sidebar.header("Grid")
 
