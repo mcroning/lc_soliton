1605abe9592daf2a2ef7194e357abd933ca04a79
Mark Cronin-Golomb <mark.cronin-golomb@tufts.edu>
2026-06-20 06:29:30 -0400

    Remove obsolete plotting and dual-grid API exports

--- src/lc_soliton/plotting.py
+++
@@ -1,8 +0,0 @@
-"""
-Plotting and visualization helpers.
-"""
-
-from .legacy_validated.lc_core.plotting import *
-from .legacy_validated.lc_core.preview import *
-from .legacy_validated.lc_core.notebook_viz import *
-
