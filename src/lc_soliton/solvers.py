5763dd81cfb011a1c7c20e49c39e1d7a304bbf27
Mark Cronin-Golomb <mark.cronin-golomb@tufts.edu>
2026-06-20 06:16:03 -0400

    Remove unused solver facade and debug output

--- src/lc_soliton/solvers.py
+++
@@ -1,9 +0,0 @@
-"""
-Static and time-dependent LC solver entry points.
-"""
-
-from .legacy_validated.lc_core.static_solver import *
-from .legacy_validated.lc_core.static_z_march import *
-from .legacy_validated.lc_core.td_runner import *
-from .legacy_validated.lc_engine_validated import *
-
